ab3d70da86fe710733b03434fc7ba4cc662f1091
Bayu <bayu@Bayus-MacBook-Air.local>
2016-11-14 11:48:31 +0700

    Change credit card Doku charge to Rp2000

--- src/app/code/community/Brander/PaymentFee/Model/Fee.php
+++ src/app/code/community/Brander/PaymentFee/Model/Fee.php
@@ -87,7 +87,7 @@ class Brander_PaymentFee_Model_Fee extends Mage_Core_Model_Abstract {
                 }
             }
 
-            return ($sum * ($fee / 100) + 3000);
+            return ($sum * ($fee / 100) + 2000);
         }
         else {
             $totals = $quote->getTotals();
